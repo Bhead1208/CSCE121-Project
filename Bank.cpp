c7fc6761d6b6b71d2b15a744c81ec6c5bbd26545
abrahamhino21 <abrahamhino21@tamu.edu>
2016-11-10 14:58:19 -0600

    Update added Deposit()

--- Bank.cpp
+++ Bank.cpp
@@ -16,6 +16,49 @@ class Bank()
 	void menu();
 	Bank();
 };
+/*
+	Function Deposit 
+	Asks for a patron ID and, if they exist, create a transaction object with the
+	attributes the patron object (reference) and "Deposit."
+	@param vactor of integers
+	@return void returns nothing
+*/
+void Bank::Deposit(vectro<int> vec){
+	int num;
+	bool valid;
+	
+	cout<<"Enter Id"<<endl;
+	cin>>num;
+	
+	// for loop checks trough vector of id's to see if id exists
+	for(int i=0 ; i<vec.size(); i++){
+		if(num == vec[i]){
+			valid = true;
+			break;
+		}
+	}
+	if(valid){
+		// getting data from user to create time object
+		cout<<"Enter Time"<<endl;
+		cout<<"Enter Hour"<<endl; 
+		cin>> int hou; // stroes hour
+		cout<<"Enter Minute"<<endl;
+		cin>> int min;// stores minute
+		cout<<"Enter Second"<<endl;
+		cin>> int sec;// stores second
+		Time timeObj(hou,min,sec);//might have built object incorrectly come back and check
+		cout<<"Enter Date"<<endl;
+		cout<<"Enter Day"<<endl; 
+		cin>> int dd; // stroes hour
+		cout<<"Enter Month"<<endl;
+		cin>> int mm;// stores minute
+		cout<<"Enter Year"<<endl;
+		cin>> int yy;// stores second
+		Date dateObj(yy,Date::Month(mm),dd); // might have built object incorrectly come back and check
+		Transaction transObj();
+	}
+	else cout<<" NO MATCHING ACCOUNT FOR THIS ID "<<endl;
+}
 
 void Bank::getTransactions()
 {
